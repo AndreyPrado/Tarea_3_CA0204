01c551396c4a351221ba75008272d46d3890d383
Adonis <adonisarayacascan@gmail.com>
2025-10-28 19:52:24 -0600

    Crear: Creacion de codigo en R sobre graficos

---
+++ Adonis_Araya_C4C517/MiTemaFavorito_AdonisAraya.R
@@ -0,0 +1,15 @@
+#se utilizara un dataset de R para mayor facilidad
+
+library(tidyverse)
+Data <-penguins 
+
+Data %>% 
+  ggplot(aes(x=flipper_len, fill = species)) +
+    geom_density(alpha=0.5) +
+      theme_minimal() +
+        labs( 
+          title = "Distribucion tamaño ala por especie",
+          y="densidad",
+          x="tamaño ala"
+            )
+  
